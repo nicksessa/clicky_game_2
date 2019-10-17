8b0776e7bc0b4ed16606c1aa4b02c3f524788e25
Nicholas Sessa <nicholas.sessa@gmail.com>
2019-10-17 19:10:59 -0400

    major fixes

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => (
-    <div className="footer fixed-bottom p-0">
+    <div className="footer p-0">
         <div className="container-fluid p-0">
             <div className="footer1 text-white pt-5"></div>
             <div className="align-text-middle text-white p-3 footer2" >
