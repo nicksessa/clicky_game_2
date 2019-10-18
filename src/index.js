251c965e05d3363dc9ca45e5ab4199ca37d3bd8e
Nicholas Sessa <nicholas.sessa@gmail.com>
2019-10-18 10:28:46 -0400

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ClickyGame from './components/ClickyGame';
-import './index.css';
-
-ReactDOM.render(<ClickyGame />, document.getElementById('root'));
